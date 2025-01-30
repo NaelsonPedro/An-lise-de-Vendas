afd7b0002920058228ca10ce1fe8a03a8fe97ae4
NaelsonP <Naelson Pedro>
2025-01-29 23:14:03 -0300

    Organização do código entre funções e adição do Readme

--- analise_vendas.py
+++ analise_vendas.py
@@ -2,40 +2,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Criando um DataFrame com dados fictícios de vendas
-dados = {
-    'Produto': ['Fones de ouvido', 'Carregadores', 'Celular', 'Capas', 'Películas'],
-    'Vendas': [15, 20, 30, 10, 25],
-    'Desconto': [10, 15, 20, 5, 10],
-    'Custo': [10, 15, 20, 5, 10]
-}
+def criar_dataframe():
+    dados = {
+        'Produto': ['Fones de ouvido', 'Carregadores', 'Celular', 'Capas', 'Películas'],
+        'Vendas': [15, 20, 30, 10, 25],
+        'Desconto': [10, 15, 20, 5, 10],
+        'Custo': [10, 15, 20, 5, 10]
+    }
+    return pd.DataFrame(dados)
 
-df = pd.DataFrame(dados)
-print(df)
 
-# Calculando o total de vendas
-total_vendas = df['Vendas'].sum()
-print(f'Total de Vendas: {total_vendas}')
+def calcular_estatisticas(df):
+    total_vendas = df['Vendas'].sum()
+    media_descontos = df['Desconto'].mean()
+    df['Lucro'] = df['Vendas'] - df['Custo']
+    return total_vendas, media_descontos
 
-# Calculando a média de descontos
-media_descontos = df['Desconto'].mean()
-print(f'Média de Descontos: {media_descontos}')
 
-# Calculando o lucro para cada produto
-df['Lucro'] = df['Vendas'] - df['Custo']
-print(df)
+def plotar_graficos(df):
+    plt.bar(df['Produto'], df['Vendas'])
+    plt.xlabel('Produto')
+    plt.ylabel('Vendas')
+    plt.title('Vendas por Produto')
+    plt.show()
 
-# Criando um gráfico de barras para visualizar as vendas
-plt.bar(df['Produto'], df['Vendas']) # plt.bar(): Cria um gráfico de barras.
-plt.xlabel('Produto')
-plt.ylabel('Vendas')
-plt.title('Vendas por Produto')
-plt.show()  # plt.show(): Exibe o gráfico.
 
-# Criando um gráfico de barras para visualizar o lucro
-plt.bar(df['Produto'], df['Lucro'], color = 'green')
-plt.xlabel('Produto')
-plt.ylabel('Lucro')
-plt.title('Lucro por Produto')
-plt.savefig('c:/Users/Naels/Envs/lucro_por_produto.png') # Salvando o gráfico como uma imagem
-plt.show()
+def main():
+    df = criar_dataframe()
+    print(df)
+    total_vendas, media_descontos = calcular_estatisticas(df)
+    print(f'Total de Vendas: {total_vendas}')
+    print(f'Média de Descontos: {media_descontos}')
+    print(df)
+    plotar_graficos(df)
+
+
+if __name__ == "__main__":
+    main()
 
